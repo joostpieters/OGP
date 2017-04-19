818316de920295748657e6e26fae94582a53ebb2
SanderLe <sander.leyssens@hotmail.com>
2017-04-19 20:40:34 +0200

    Added empty ProgramFactory, added empty constructors
    Signed-off-by: SanderLe <sander.leyssens@hotmail.com>

--- src/asteroids/model/MinorPlanet.java
+++ src/asteroids/model/MinorPlanet.java
@@ -2,6 +2,12 @@ package asteroids.model;
 
 public abstract class MinorPlanet extends Entity {
 
+	protected MinorPlanet(double x, double y, double xVelocity,
+			double yVelocity, double radius) throws IllegalArgumentException {
+		super(x, y, xVelocity, yVelocity, radius);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public double getMass() {
 		// TODO Auto-generated method stub
--- src/asteroids/model/Planetoid.java
+++ src/asteroids/model/Planetoid.java
@@ -2,4 +2,10 @@ package asteroids.model;
 
 public class Planetoid extends MinorPlanet {
 
+	protected Planetoid(double x, double y, double xVelocity, double yVelocity,
+			double radius) throws IllegalArgumentException {
+		super(x, y, xVelocity, yVelocity, radius);
+		// TODO Auto-generated constructor stub
+	}
+
 }
