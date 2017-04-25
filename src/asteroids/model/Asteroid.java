491fc6d12e63be83a86f648e624ea8482a69cbf8
sjoseph22 <sarah_joseph@live.com>
2017-04-25 19:04:44 +0200

    asteroid update

--- src/asteroids/model/Asteroid.java
+++ src/asteroids/model/Asteroid.java
@@ -9,17 +9,40 @@ public class Asteroid extends MinorPlanet {
 	}
 
 	@Override
-	public double getDensity() {
-		// TODO Auto-generated method stub
-		return 0;
+	public double getRadius() {
+		return radius;
 	}
+	
+	private static final double radius = 5;
+
+	@Override
+	public double getDensity() {
+		return density;
+	}
+	
+	private static final double density = 0.917E12;
 
 	@Override
 	public void collide(Entity entity) {
 		if (entity instanceof Ship) {
-			// TODO
-		}
+			double mi = this.getMass();double mj = entity.getMass();
+			double[] deltaR = this.getPositionDifference(entity);
+			double[] deltaV = this.getVelocityDifference(entity);
+			double sigma = dotProduct(deltaR,deltaR);
+			double j = 2*mi*mj*(dotProduct(deltaV,deltaR))/(sigma*(mi+mj));
+			double jx = j*deltaR[0]/sigma;double jy = j*deltaR[1]/sigma;
+			double[] oldVelocityi = this.getVelocity();
+			double[] oldVelocityj = entity.getVelocity();
+			double[] newVelocityi = new double[]{oldVelocityi[0]+jx/mi,oldVelocityi[1]+jy/mi};
+			double[] newVelocityj = new double[]{oldVelocityj[0]-jx/mj,oldVelocityj[1]-jy/mj};
+			this.setVelocity(newVelocityi);entity.setVelocity(newVelocityj);		}
 		else super.collide(entity);
 	}
+	
+	public void move(double dt) throws IllegalArgumentException {
+		setPosition(getPositionAfterMovingForAPeriodOf(dt));
+	}
 
+	
+	
 }
